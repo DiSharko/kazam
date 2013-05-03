987d39ea863909c2b8521774710c842d8b60b14b
Christopher Morris <cjm5@cs.brown.edu>
2013-05-03 12:36:35 -0400

    Added complete network code.

---
+++ src/network/StateHandler.java
@@ -0,0 +1,47 @@
+package network;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class StateHandler extends Thread {
+	private Socket _client;
+	private String _message;
+	private ClientPool _clients;
+
+	public StateHandler(Socket client, String message, ClientPool clients) {
+		_client = client;
+		_message = message;
+		_clients = clients;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			synchronized(_client) {
+				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(_client.getOutputStream()));
+				writer.write(_message);
+				writer.write("EOT\n");
+				writer.flush();
+			}
+		} catch (IOException e) {
+			closeSocket();
+		}
+	}
+	
+	public void closeSocket() {
+		if (!_client.isClosed()) {
+			try {
+				_client.close();
+			} catch (IOException e) {
+				// ignore as disconnecting
+			}
+		}
+		// remove from list - this should block until broadcast() or broadcastStart() is finished
+		synchronized(_clients) {
+			_clients.remove(_client);
+		}
+	}
+
+}
