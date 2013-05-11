f0f80a0be2d1624945ea7a44309d6ff70450a089
Andrew DiMarco <dimarc217@gmail.com>
2013-05-11 11:49:44 -0400

    tests and endscreen

--- src/pvpmagic/EndScreen.java
+++ src/pvpmagic/EndScreen.java
@@ -1,5 +1,6 @@
 package pvpmagic;
 
+import java.awt.Font;
 import java.awt.Graphics2D;
 
 import screen.Screen;
@@ -7,11 +8,12 @@ import screen.ScreenHolder;
 
 public class EndScreen extends Screen {
 
+	boolean _winner = false;
+	
 	public EndScreen(ScreenHolder holder) {
 		super(holder, "end");
 		
 		
-	
 	}
 
 	@Override
@@ -21,7 +23,15 @@ public class EndScreen extends Screen {
 
 	@Override
 	protected void draw(Graphics2D g) {
-		
+		if (_winner){
+			g.setFont(new Font("Times New Roman", Font.PLAIN, 36));
+			
+			String s = "Victory!";
+			int sWidth = (int)g.getFontMetrics().getStringBounds(s, g).getWidth();
+			g.drawString(s, _holder._w/2-sWidth/2, 100);
+		} else {
+			
+		}
 	}
 
 	@Override
